4c664c3d2b32fe9bd03d0c239573ae30a1cd6b19
Valentina <valentina@oyga.me>
2021-12-13 19:44:13 -0300

    initial commit

---
+++ apps/todos/src/app/app.tsx
@@ -0,0 +1,13 @@
+import styles from './app.module.css';
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <>
+      <NxWelcome title="todos" />
+      <div />
+    </>
+  );
+}
+
+export default App;
